7e0ead693bce923da2ae79d46c04dd504cfbee61
maysing <93945211+maysing@users.noreply.github.com>
2024-03-23 14:55:55 +0100

    touch collapse

--- main.js
+++ main.js
@@ -2,28 +2,32 @@ const hamburger = document.querySelector(".hamburger");
 const verkButton = document.querySelector(".verk-btn");
 const nav = document.querySelector("nav ul .verk-show");
 const hamMenu = document.querySelector(".hamburger-menu");
-const verkBtnHeader = document.querySelector(".verksamhet-btn-header")
-const verkSubSections = document.querySelector(".sub-sections")
+const verkBtnHeader = document.querySelector(".verksamhet-btn-header");
+const verkSubSections = document.querySelector(".sub-sections");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   hamMenu.classList.toggle("active");
-  
 });
 
 verkButton.addEventListener("click", () => {
-    nav.classList.toggle("show");
+  nav.classList.toggle("show");
 });
 
 verkBtnHeader.addEventListener("click", () => {
-  verkSubSections.classList.toggle ("show");
+  verkSubSections.classList.toggle("show");
 });
 
-
-document.addEventListener('click', function(event) {
-
+document.addEventListener("click", function (event) {
   if (!hamMenu.contains(event.target) && !hamburger.contains(event.target)) {
-      hamMenu.classList.remove('active');
-      hamburger.classList.remove('active');
+    hamMenu.classList.remove("active");
+    hamburger.classList.remove("active");
+  }
+});
+
+document.addEventListener("touchstart", function (event) {
+  if (!hamMenu.contains(event.target) && !hamburger.contains(event.target)) {
+    hamMenu.classList.remove("active");
+    hamburger.classList.remove("active");
   }
 });
